16ba576e33b82e2fec7c4bc7cd9d9d26c5375449
Tim Spain <timothy.spain@nersc.no>
2021-07-26 14:12:51 +0100

    Compile and link NetCDF using CMake, and access in hello

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <netcdf>
+
+// We are writing 2D data, a 6 x 12 grid.
+static const int NX = 6;
+static const int NY = 12;
 
 int main( ) {
+
+	int data[NX][NY];
+
+	netCDF::NcFile file;
 	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
